6099ffdce611e07831d82d58701e3baf41873a42
Zach Saffrin <zach@zachsaffrin.com>
2018-12-23 15:27:16 -0500

    Update chucktionapi.js

--- src/functions/chucktionapi.js
+++ src/functions/chucktionapi.js
@@ -2,11 +2,23 @@ import axios from 'axios';
 
 const API_ENDPOINT = 'https://chucktionapi.firebaseapp.com/api/v1/';
 
-exports.handler = (event, context) =>
-  axios
-    .get(API_ENDPOINT)
-    .then(res => ({ statusCode: 200, body: res.data }))
-    .catch(err => {
-      console.error(err);
-      return { statusCode: 422, body: String(err) };
-    });
+exports.handler = async (event, context) => {
+  try {
+    const response = await axios.get(API_ENDPOINT);
+    if (!response.ok) {
+      return { statusCode: response.status, body: response.statusText };
+    }
+    const data = await response.data;
+
+    return {
+      statusCode: 200,
+      body: data,
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: String(err),
+    };
+  }
+};
