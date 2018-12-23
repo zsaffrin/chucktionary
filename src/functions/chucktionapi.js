1d82d6be9cb70b7186a4130ffcec802f82ca8ccf
Zach Saffrin <zach@zachsaffrin.com>
2018-12-23 15:48:51 -0500

    Update chucktionapi.js

--- src/functions/chucktionapi.js
+++ src/functions/chucktionapi.js
@@ -1,10 +1,8 @@
-const axios = require('axios');
-
 const API_ENDPOINT = 'https://chucktionapi.firebaseapp.com/api/v1/';
 
 exports.handler = async function(event, context) {
   try {
-    const response = await axios.get(API_ENDPOINT);
+    const response = await fetch(API_ENDPOINT);
     if (!response.ok) {
       return { statusCode: response.status, body: response.statusText };
     }
